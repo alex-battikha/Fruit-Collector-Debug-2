7fa9be802e1dc155947354b239beda53deee411b
alex-battikha <56514843+alex-battikha@users.noreply.github.com>
2021-05-17 00:34:36 -0700

    working score

--- js/game.js
+++ js/game.js
@@ -53,36 +53,57 @@ class Game {
         players[index - 1].x = x;
         players[index - 1].y = y;
         
-        if(index === player.index){    
+        if(index === player.index){
+            textFont(antonFont);    
             fill("black");
             textSize(25);
             text(allPlayers[plr].name, x-25, y+25);
-        }    
+        }
+
+        textFont(antonFont);
+        textSize(34);
+        fill(255);
+
+        //Method #1 for displaying the score
+        // text("Player 1: " + allPlayers.player1.score, 40, 60);
+        // text("Player 2: " + allPlayers.player2.score, 50, 110);
+
+        //Method #2 for displaying the score
+        if(index === 1) {
+            text("Player 1: " + allPlayers[plr].score, 40, 60);
+        }
+        //is only ran on the second time going through the loop
+        else {
+            text("Player 2: " + allPlayers[plr].score, 50, 110);
+        }
     }
 
-    if (keyIsDown(RIGHT_ARROW) && player.index !== null) {
+    if (keyIsDown(RIGHT_ARROW) && player.index !== null && player.distance > -410) {
         player.distance -= 10;
         player.update();
     }
-    if (keyIsDown(LEFT_ARROW) && player.index !== null) {
+
+    if (keyIsDown(LEFT_ARROW) && player.index !== null && player.distance < 410) {
         player.distance += 10;
         player.update();
     }
+    console.log(player1.x);
 
     if (frameCount % 20 === 0) {
         fruits = createSprite(random(100, 1000), 0, 100, 100);
         fruits.velocityY = 6;
+        fruits.lifetime = 125;
         var rand = Math.round(random(1,5));
         switch(rand){
             case 1: fruits.addImage("fruit1",fruit1_img);
             break;
-            case 2: fruits.addImage("fruit1", fruit2_img);
+            case 2: fruits.addImage("fruit2", fruit2_img);
             break;
-            case 3: fruits.addImage("fruit1", fruit3_img);
+            case 3: fruits.addImage("fruit3", fruit3_img);
             break;
-            case 4: fruits.addImage("fruit1", fruit4_img);
+            case 4: fruits.addImage("fruit4", fruit4_img);
             break;
-            case 5: fruits.addImage("fruit1", fruit5_img);
+            case 5: fruits.addImage("fruit5", fruit5_img);
             break;
         }
         fruitGroup.add(fruits);
